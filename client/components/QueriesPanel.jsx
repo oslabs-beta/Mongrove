e2331231314e631b7b8e17cd88d5c862ac61809e
YaelWOfer <68725542+YaelWOfer@users.noreply.github.com>
2021-06-17 12:39:32 -0700

    Corrects queriesPanel functionality

--- client/components/QueriesPanel.jsx
+++ client/components/QueriesPanel.jsx
@@ -4,7 +4,7 @@ const QueriesPanel = (props) => {
     //create an array to hold all queryItems
     const queryItemsList = []
     
-    for (let i=0; i<props.qu){
+    for (let i=0; i < props.testQueriesList.length; i++){
         queryItemsList.push(<QueryItem key={i} id={i} queryName={props.testQueriesList[i].name}/>)
     }
     return (
