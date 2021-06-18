0c386a99e547c77038d1081140e5c00ae87683b1
Sushanth Kambham <sushanth9@hotmail.com>
2021-06-17 18:32:26 -0700

    Adding mvp demo files

--- client/pages/SchemaMainArea.jsx
+++ client/pages/SchemaMainArea.jsx
@@ -6,7 +6,7 @@ import TestDBGenArea from './TestDBGenArea.jsx';
 const SchemaMainArea = (props) => {
 // use useState for handling schema data from SchemaArea component to passdown to dropdown selection in TestDBGenArea    
     return (
-        <div id="mainArea" className="mainAreaComponents">
+        <div id="mainArea" className="mainAreaContainers">
             <SchemaArea {...props} />
             <TestDBGenArea {...props}/>
         </div>
