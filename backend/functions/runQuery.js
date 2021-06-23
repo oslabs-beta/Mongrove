06dabc778ba9faecec50998963999ddd5f5f3226
Peter Van <peterrvan@gmail.com>
2021-06-23 12:17:37 -0700

    Moved generateSchema functionality to runQuery. App no longer generates a new file for a new schema. Fixed error when running two different queries in a row for the same database.

--- backend/functions/runQuery.js
+++ backend/functions/runQuery.js
@@ -1,19 +1,40 @@
 const { PerformanceObserver, performance } = require('perf_hooks');
 const generateTestDatabase = require('./generateTestDatabase');
 const path = require('path');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-async function runQuery(query, schemaName, schema, numberOfDocuments) {
-  console.log('dirname', __dirname);
-  console.log(path.join(__dirname, `../backend/models/${schemaName}`))
-  const model = require(`../models/${schemaName}`);
+
+async function runQuery(query, schemaName, schema, numberOfDocuments, databaseName) {
+
+  // const model = require(`../models/${schemaName}`);
   // Call function to generate test database with dummy data
+  await mongoose.connect('mongodb://localhost:27017', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    dbName: databaseName
+  })
+
+  const testSchema = eval(`new Schema(${schema})`);
+
+  let model;
+  try {
+    if (mongoose.model(`${schemaName}`)) model = mongoose.model(`${schemaName}`);
+  } catch (err) {
+    model = mongoose.model(schemaName, testSchema, schemaName);
+  }
+
   const data = generateTestDatabase(schema, numberOfDocuments);
   
-  model.insertMany(data, function (err) {
-    if (err) return console.log('Error: ', err);
+  try{
+    await model.insertMany(data),
     console.log('Data added!');
-  });
-
+  } catch (err) {
+    console.log('Error in inserting data', err);
+  }
+  
   const time1 = performance.now();
 
   try {
@@ -26,11 +47,15 @@ async function runQuery(query, schemaName, schema, numberOfDocuments) {
   const responseTime = time2 - time1;
 
   try {
-    await model.deleteMany({});
+    await model.deleteMany({}).then(data => {
+      console.log("deleted items", data);
+
+    });
   } catch (err) {
     console.log('Error in deleting database', err);
   }
   console.log('response time: ', responseTime);
+  mongoose.connection.close();
   return responseTime;
 }
 
