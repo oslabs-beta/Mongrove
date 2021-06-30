6c36856d7e4dcd11599e8460109925dfe6531eb8
YaelWOfer <68725542+YaelWOfer@users.noreply.github.com>
2021-06-29 21:21:01 -0700

    Adds styling to help modal

--- backend/channels.js
+++ backend/channels.js
@@ -22,8 +22,8 @@ ipcMain.handle('generate-test-data', async (event, ...args) => {
 ipcMain.on('open-help', function(){
   // Create the browser window.
   let win = new BrowserWindow({
-    width: 400,
-    height: 400,
+    width: 500,
+    height: 800,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
